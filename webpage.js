6f77237f09b706960f32774a44ce0aea364ee44d
abonagiri <104653601+4uOfc@users.noreply.github.com>
2024-07-17 14:28:47 -0500

    Update webpage.js

--- webpage.js
+++ webpage.js
@@ -35,23 +35,19 @@ TESTER = document.getElementById("tester");
 Plotly.newPlot(
   TESTER,
   [
-    {
-      x: ['red', 'green', 'orange', 'yellow', 'purple'],
-      y: [1, 2, 4, 8, 16],
-    type: 'bar'
-    }
-  ],
-  layout
-);
-
-var data = [
-  {
     x: ['Blue', 'Cyan', 'Green', 'Yellow', 'Orange', 'Red', 'Deep Red'],
     y: [23.98523985, 12.06140351, 15.12605042, 14.07657658, 15.87837838, 33.95784543, 54.57920792].map(num => parseFloat(num.toPrecision(4))),
     type: 'bar',
     marker: {
       color: ['blue', 'cyan', 'green', 'yellow', 'orange', 'red', 'darkred']
     }
-  }
-];
+  ],
+  layout
+);
 Plotly.newPlot('data', data);
+
+var greenBtn = document.querySelector('button');
+
+greenBtn.addEventListener('click', function() {
+  greenBtn.style.background = 'red';
+})
